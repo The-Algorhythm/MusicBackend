8bc3d3784dbd17e2db6edaf38b6ba77e00b29bd2
David <dad9489@rit.edu>
2020-10-23 02:09:35 -0400

    change redirect url to work dependent on host url

--- MusicBackend/routes/auth.py
+++ MusicBackend/routes/auth.py
@@ -4,19 +4,25 @@ from spotipy.oauth2 import SpotifyOAuth
 from spotipy import Spotify
 from django.http import HttpResponseRedirect
 
-redirect_uri = 'http://localhost:8000/login'
-scopes = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative'
-sp_oauth = SpotifyOAuth(scope=scopes,
-                            client_id=os.getenv("SPOTIFY_CLIENT_ID"),
-                            client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
-                            redirect_uri=redirect_uri)
+class SpotifyCommunicator:
 
+    def __init__(self):
+        self.scopes = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative'
+        self.sp_oauth = None
+        self.sp = None
 
-def initalize_auth(request):
-    return HttpResponseRedirect(sp_oauth.get_authorize_url())
+    def _initOAuth(self, redirect_uri):
+        self.sp_oauth = SpotifyOAuth(scope=self.scopes,
+                                     client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+                                     client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+                                     redirect_uri=redirect_uri)
+        return self.sp_oauth.get_authorize_url()
 
+    def initalize_auth(self, request):
+        redirect_uri = request.build_absolute_uri('/')+'login'
+        return HttpResponseRedirect(self._initOAuth(redirect_uri))
 
-def test_spotify(request, token):
-    token_info = sp_oauth.get_access_token(token)
-    sp = Spotify(token_info['access_token'])
-    return sp.current_user()
+    def test_spotify(self, request, token):
+        token_info = self.sp_oauth.get_access_token(token)
+        self.sp = Spotify(token_info['access_token'])
+        return self.sp.current_user()
--- MusicBackend/routes/index.py
+++ MusicBackend/routes/index.py
@@ -5,15 +5,17 @@ import json
 
 from ..routes.auth import *
 
+spotify = SpotifyCommunicator()
+
 
 def get_index(request):
     if request.GET.get('code') is not None:  # triggered on redirect with auth token
         request.session['token'] = request.GET.get('code')
         return HttpResponseRedirect("/")
     if request.session.get('token') is None:  # triggered when user who is not logged in requests site
-        return initalize_auth(request)
+        return spotify.initalize_auth(request)
 
-    user = test_spotify(request, request.session.get('token'))
+    user = spotify.test_spotify(request, request.session.get('token'))
     return JsonResponse({'current_user': user})
 
 
