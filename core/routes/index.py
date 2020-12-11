c3d83b4ee811e0f4fb3b241b07cd7abef2e10648
David <dad9489@rit.edu>
2020-12-10 21:46:26 -0500

    Back to cookies :P

--- core/routes/index.py
+++ core/routes/index.py
@@ -10,13 +10,15 @@ spotify = SpotifyCommunicator()
 
 def get_index(request):
     if request.GET.get('code') is not None:  # triggered on redirect with auth token
-        request.session['token_info'] = spotify.get_initial_token_info(request.GET.get('code'))
-        return HttpResponseRedirect("/")
-    if request.session.get('token_info') is None:  # triggered when user who is not logged in requests site
+        response = HttpResponseRedirect("/")
+        token_info = spotify.get_initial_token_info(request.GET.get('code'))
+        response.set_cookie('token_info', json.dumps(token_info))
+        return response
+    if request.COOKIES.get('token_info') is None:  # triggered when user who is not logged in requests site
         return spotify.initalize_auth(request)
 
     spotify.initalize_auth(request)  # initalize oauth client if not done already
-    token_info = spotify.update_token_info(request.session.get('token_info'))
+    token_info = spotify.update_token_info(json.loads(request.COOKIES.get('token_info')))
     user = spotify.test_spotify(token_info)
     return JsonResponse({'current_user': user})
 
